6b91c540e524b5f370cf046d14b31a3b75d2157e
safiiaaa <165434516+safiiaaa@users.noreply.github.com>
2024-07-21 14:05:33 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.btn').forEach(button => {
-        button.addEventListener('click', function() {
-            alert(`You clicked: ${this.textContent}`);
-        });
-    });
-});
